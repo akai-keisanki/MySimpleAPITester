65eba9575c95d191012fcba20bb5640691a8080a
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2024-12-31 14:50:20 -0300

    feat: added dotenv parameters and parameter visualisation.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,33 @@
 from secondary import *
 
-
 def main() -> None:
 
-    op : str
+    dotenv.load_dotenv()
 
+    op : str
     r : requests.models.Response
 
-    while True:
+    s : requests.Session = requests.Session()
 
-        s = requests.Session()
+    while True:
 
         op = input('OP: ')
 
         if op in ['q', 'quit', 'exit']: break
 
-        elif op in ['n', 'new', 't', 'test']:
+        elif op in ['n', 'new', 't', 'test', 'r', 'request', 'new-request', 'new-req', 'test-request', 'test-req']:
             show_req(req(s, input('-> Method  : '), input('-> URL     : '), input('-> Params  : '), input('-> JSON    : ')))
         
-        elif op in ['sp', 'section-parameters', 'section-params']:
+        elif op in ['sp', 'section-parameters', 'section-params', 's-params']:
             try: s.params = eval(input('-> Params  : '))
             except: print('Error while evaluating parameters')
+
+        elif op in ['ssp', 'show-section-parameters', 'show-section-params', 'show-s-params']:
+            print(f'-- sec.par.: {s.params}')
+
+        elif op in ['de', 'dotenv', 'get-dotenv', 'dotenv-params', 'desp', '.env']:
+            try: s.params[input('-> label   : ')] = os.environ.get(input('-> .env var: '))
+            except: print('Error while getting parameters')
 
     return
 
--- src/terciary.py
+++ src/terciary.py
@@ -1,3 +1,5 @@
+import os
+import dotenv
 import requests
 
 def req_mets(s : requests.Session) -> dict[callable]:
